31d55776fa9ed43e72f8eed7cb6ed029cde2b3fd
Guilherme Oliveira <oliveira.g.ar@proton.me>
2024-04-11 21:48:22 -0300

    add unit tests

--- test_sentiment_analysis.py
+++ test_sentiment_analysis.py
@@ -1,2 +1,13 @@
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 import unittest
+
+class TestSentimentAnalyzer(unittest.TestCase):
+    def test_sentiment_analyzer(self):
+        result_1 = sentiment_analyzer("I love working with Python")
+        self.assertEqual(result_1['label'], "SENT_POSITIVE") # unit tests
+        result_2 = sentiment_analyzer("I hate working with Pyhton")
+        self.assertEqual(result_2['label'], "SENT_NEGATIVE")
+        result_3 = sentiment_analyzer("I am neutral on Python")
+        self.assertEqual(result_3['label'], "SENT_NEUTRAL")
+
+unittest.main() # call the unit tests
