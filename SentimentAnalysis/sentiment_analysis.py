2f298f8df6403f763414725b6de7dc590498a2b2
Guilherme Oliveira <oliveira.g.ar@proton.me>
2024-04-12 22:01:34 -0300

    add error handling on server.py and sentiment_analysis.py

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,24 +1,42 @@
-import requests
-import json
-from ibm_watson import NaturalLanguageUnderstandingV1
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
+"""
+Module that executes a sentiment analysis
+from any text, and return a score.
+"""
 
+import json
+import requests
+from ibm_watson import NaturalLanguageUnderstandingV1,SpeechToTextV1, TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+# from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
+
+# Authentication via IAM
 authenticator = IAMAuthenticator('b2GhucMwzU7Pp131eLvW2ZvvH3VF-ZwgA4VHzCGCQs7G')
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2022-04-07',
-    authenticator=authenticator
-)
+    authenticator=authenticator)
+# service.set_service_url('https://gateway.watsonplatform.net/natural-language-understanding/api')
+text_to_speech = TextToSpeechV1(authenticator=authenticator)
+spech_to_test = SpeechToTextV1(authenticator=authenticator)
 
-natural_language_understanding.set_service_url('https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/1cd88e19-aef4-4606-b7fc-abdfad735206')
 
 def sentiment_analyzer(text_to_analyse): # POST request
+    """
+        send a POST request to the relevant model 
+        with the required text and the model will send the appropriate response
+    """
+    url = (
+        'https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com'
+        '/instances/1cd88e19-aef4-4606-b7fc-abdfad735206'
+    )
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    response = natural_language_understanding.analyze(
-        url = 'https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/1cd88e19-aef4-4606-b7fc-abdfad735206',
-        json = myobj, headers=header, features=Features(sentiment=SentimentOptions(model='bert'))).get_result()
+    response = requests.post(url, timeout=10, json = myobj, headers=header)
+    #features=Features(sentiment=SentimentOptions(model='bert')).get_result()
     formatted_response = json.loads(response.text) # convert text into a dictionary
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
     return {'label': label, 'score': score}
--- server.py
+++ server.py
@@ -2,9 +2,7 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package : TODO
-from flask import Flask, render_template, request, jsonify
-# Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 #Initiate the flask app : TODO
@@ -21,19 +19,17 @@ def sent_analyzer():
     response = sentiment_analyzer(text_to_analyze)
     label = response['label']
     score = response['score']
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    if label is None:
+        return "Invalid input ! Try again."
+        # include a special case, where a blank input receives a different error message : TODO
+    return f'The given text has been identified as {label.split("_")[1]} with a score of {score}'
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
+    ''' This function initiates the rendering of the 
+        main application page over the Flask channel.
     '''
-    #TODO
     return render_template("index.html")
 
 if __name__ == "__main__":
-    ''' 
-    This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(host="0.0.0.0", port=5000)
-    
